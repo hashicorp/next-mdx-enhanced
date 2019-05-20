e71e8a8f3218dcac8f56fb82207644d680cfb44e
Kevin Pruett <pruett.kevin@gmail.com>
2019-05-20 15:56:52 -0600

    Refactor plugin to use OS packages

--- index.js
+++ index.js
@@ -1,65 +1,96 @@
+const fs = require('fs')
+const matter = require('gray-matter')
 const path = require('path')
-const babelPluginFrontmatter = require('./babelPlugin')
-const frontmatterExtractPlugin = require('./plugin')
+const withPrebuild = require('@hashicorp/next-prebuild')
 const { createConfigItem } = require('@babel/core')
+const { generateFrontmatterPath } = require('./util')
+const babelPluginFrontmatter = require('./plugin')
 
-module.exports = function mdxEnhancedPlugin(pluginOptions = {}) {
+function extractFrontMatter(files, root) {
+  return Promise.all(files.map(f => fs.readFile(f, 'utf8')))
+    .then(fileContents => {
+      const fmPaths = files.map(f =>
+        generateFrontmatterPath(f, this.nextConfig)
+      )
+      const frontMatter = fileContents.map((content, idx) => {
+        return {
+          ...matter(content).data,
+          __resourcePath: files[idx].replace(path.join(root, 'pages'), '')
+        }
+      })
+      return Promise.all(
+        fmPaths.map(fmPath => fs.ensureDir(path.dirname(fmPath)))
+      ).then(() => [frontMatter, fmPaths])
+    })
+    .then(([contents, fmPaths]) => {
+      return Promise.all(
+        contents.map((content, idx) => {
+          fs.writeFile(fmPaths[idx], JSON.stringify(content))
+        })
+      )
+    })
+}
+
+const withEnhancedMdx = (pluginOptions = {}) => (nextConfig = {}) => {
   if (!pluginOptions.layoutPath) pluginOptions.layoutPath = 'layouts'
 
-  return (nextConfig = {}) => {
-    // We want to add "mdx" to the page extensions but also don't want to
-    // clobber any pre-existing extensions configured.
-    if (!nextConfig.pageExtensions) {
-      nextConfig.pageExtensions = ['js', 'jsx']
-    }
-
-    if (nextConfig.pageExtensions.indexOf('mdx') === -1) {
-      nextConfig.pageExtensions.unshift('mdx')
-    }
-
-    return Object.assign({}, nextConfig, {
-      webpack(config, options) {
-        // Add mdx loaders to webpack rules
-        config.module.rules.push({
-          test: /\.mdx?$/,
-          use: [
-            options.defaultLoaders.babel,
-            '@mdx-js/loader',
-            {
-              loader: path.join(__dirname, './loader'),
-              options: Object.assign({}, options, {
-                mdxEnhancedPluginOptions: pluginOptions
-              })
-            }
-          ]
-        })
-
-        // Frontmatter must be extracted in a separate plugin hook that runs before loaders.
-        // Webpack does not process files in a specific order, but we must guarantee that
-        // frontmatter has all been extracted before files that could potentially require it
-        // are evaluated through the loader. More details in the plugin source.
-        config.plugins.push(new frontmatterExtractPlugin(options))
-
-        // Inject the frontmatter extraction babel plugin
-        config.module.rules = config.module.rules.map(rule => {
-          if (rule.use.loader === 'next-babel-loader') {
-            if (!rule.use.options.plugins) rule.use.options.plugins = []
-            rule.use.options.plugins.push(
-              // in theory we can pass options directly to the plugin rather than wrapping
-              createConfigItem(babelPluginFrontmatter(options))
-            )
-          }
-          return rule
-        })
-
-        // If the config already has a webpack configuration, then we want to
-        // call that to do their customizations.
-        if (typeof nextConfig.webpack === 'function') {
-          return nextConfig.webpack(config, options)
-        }
-
-        return config
-      }
-    })
+  if (!nextConfig.pageExtensions) {
+    nextConfig.pageExtensions = ['jsx', 'js']
   }
+
+  if (nextConfig.pageExtensions.indexOf('mdx') === -1) {
+    nextConfig.pageExtensions.unshift('mdx')
+  }
+
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.mdx?$/,
+        use: [
+          options.defaultLoaders.babel,
+          '@mdx-js/loader',
+          {
+            loader: path.join(__dirname, './loader'),
+            options: Object.assign({}, options, {
+              mdxEnhancedPluginOptions: pluginOptions
+            })
+          }
+        ]
+      })
+
+      config.module.rules = config.module.rules.map(rule => {
+        if (rule.use.loader === 'next-babel-loader') {
+          if (!rule.use.options.plugins) rule.use.options.plugins = []
+          rule.use.options.plugins.push(
+            createConfigItem(babelPluginFrontmatter(options))
+          )
+        }
+        return rule
+      })
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    }
+  })
+}
+
+module.exports = (pluginOptions = {}) => (nextConfig = {}) => {
+  return [
+    withPrebuild({
+      build: (_, compilation, files) => {
+        extractFrontMatter(files, compilation.context)
+      },
+      watch: (_, compilation, files) => {
+        extractFrontMatter(files, compilation.context)
+      },
+      files: {
+        pattern: 'pages/**/*.mdx',
+        addFilesAsDependencies: true
+      }
+    }),
+    withEnhancedMdx(pluginOptions)
+  ].reduce((acc, next) => next(acc), nextConfig)
 }
