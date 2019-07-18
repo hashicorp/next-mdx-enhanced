39e0e536eaf051c795bbd386c8f65b8a68daec3b
Jeff Escalante <gh.je@mailhero.io>
2019-07-18 14:22:07 -0400

    update deps, fix tests

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -11,25 +11,13 @@ test('basic integration test', async () => {
   const basicFixture = path.join(__dirname, 'fixtures/basic')
   const outPath = await compileNextjs(basicFixture)
   expectContentMatch(outPath, 'index.html', /Hello world/)
+  expectContentMatch(outPath, 'docs/advanced.html', /<p>LAYOUT TEMPLATE<\/p>/)
+  expectContentMatch(outPath, 'docs/advanced.html', /<h1>Advanced Docs<\/h1>/)
+  expectContentMatch(outPath, 'docs/intro.html', /<p>LAYOUT TEMPLATE<\/p>/)
+  expectContentMatch(outPath, 'docs/intro.html', /<h1>Intro Docs<\/h1>/)
   expectContentMatch(
     outPath,
-    'docs/advanced/index.html',
-    /<p>LAYOUT TEMPLATE<\/p>/
-  )
-  expectContentMatch(
-    outPath,
-    'docs/advanced/index.html',
-    /<h1>Advanced Docs<\/h1>/
-  )
-  expectContentMatch(
-    outPath,
-    'docs/intro/index.html',
-    /<p>LAYOUT TEMPLATE<\/p>/
-  )
-  expectContentMatch(outPath, 'docs/intro/index.html', /<h1>Intro Docs<\/h1>/)
-  expectContentMatch(
-    outPath,
-    'docs/intro/index.html',
+    'docs/intro.html',
     /some <em>introductory<\/em> docs content/
   )
 })
@@ -38,25 +26,13 @@ test('layoutPath and defaultLayout options', async () => {
   const layoutsPathFixture = path.join(__dirname, 'fixtures/layouts-path')
   const outPath = await compileNextjs(layoutsPathFixture)
   expectContentMatch(outPath, 'index.html', /Hello world/)
+  expectContentMatch(outPath, 'docs/advanced.html', /<p>LAYOUT TEMPLATE<\/p>/)
+  expectContentMatch(outPath, 'docs/advanced.html', /<h1>Advanced Docs<\/h1>/)
+  expectContentMatch(outPath, 'docs/intro.html', /<p>LAYOUT TEMPLATE<\/p>/)
+  expectContentMatch(outPath, 'docs/intro.html', /<h1>Intro Docs<\/h1>/)
   expectContentMatch(
     outPath,
-    'docs/advanced/index.html',
-    /<p>LAYOUT TEMPLATE<\/p>/
-  )
-  expectContentMatch(
-    outPath,
-    'docs/advanced/index.html',
-    /<h1>Advanced Docs<\/h1>/
-  )
-  expectContentMatch(
-    outPath,
-    'docs/intro/index.html',
-    /<p>LAYOUT TEMPLATE<\/p>/
-  )
-  expectContentMatch(outPath, 'docs/intro/index.html', /<h1>Intro Docs<\/h1>/)
-  expectContentMatch(
-    outPath,
-    'docs/intro/index.html',
+    'docs/intro.html',
     /some <em>introductory<\/em> docs content/
   )
 })
@@ -80,7 +56,7 @@ afterAll(() => {
   return Promise.all([
     rmfr(path.join(__dirname, 'fixtures/*/out'), { glob: true }),
     rmfr(path.join(__dirname, 'fixtures/*/.mdx-data'), { glob: true }),
-    rmfr(path.join(__dirname, 'fixtures/*/.next'), { glob: true }),
+    rmfr(path.join(__dirname, 'fixtures/*/.next'), { glob: true })
   ])
 })
 
@@ -93,7 +69,7 @@ function compileNextjs(projectPath, configPath = 'next.config.js') {
     .then(() => {
       return nextExport(projectPath, {
         outdir: outPath,
-        silent: true,
+        silent: true
       })
     })
     .then(() => outPath)
