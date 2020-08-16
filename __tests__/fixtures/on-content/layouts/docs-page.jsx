03aaa88d2ee8d057642f2ff545b990fc3193669f
Jem (Jam) Bezooyen <hey@jem.dev>
2020-08-15 19:08:24 -0700

    fix(tests): Update tests to work with new layouts
    This branch shifted the default layout format to a file that exports a
    single functional component as default (wrather than returning a
    function that THEN returns the component).
    
    Tests however had the old layouts and all need updating
    
    BREAKING CHANGE: Layouts having a new api means they will need updating,
    and currently this doesn't support a layout also doing any sort of custom
    data fetching (such as `getStaticProps` and the like).

--- __tests__/fixtures/on-content/layouts/docs-page.jsx
+++ __tests__/fixtures/on-content/layouts/docs-page.jsx
@@ -1,5 +1,3 @@
-export default function Page() {
-  return function docsPageLayout({ children }) {
-    return <>{children}</>
-  }
+export default function docsPageLayout({ children }) {
+  return <>{children}</>
 }
--- __tests__/fixtures/scan-mdx-content/layouts/docs-page.jsx
+++ __tests__/fixtures/scan-mdx-content/layouts/docs-page.jsx
@@ -1,30 +1,29 @@
 import { frontMatter as introData } from '../pages/docs/intro.mdx'
 import { frontMatter as advancedData } from '../pages/docs/advanced.mdx'
-export default (frontMatter) => {
+
+export default function docsPageLayout({ children, frontMatter }) {
   const __scans = frontMatter.__scans
-  return function docsPageLayout({ children }) {
-    return (
-      <>
-        {/* Similar to adding a script to document <head/> we load a font stylesheet here vs. running arbitrary JS */}
-        {__scans.hasSnargles && (
-          <link
-            href="https://fonts.googleapis.com/css?family=Press+Start+2P"
-            rel="stylesheet"
-          />
-        )}
+  return (
+    <>
+      {/* Similar to adding a script to document <head/> we load a font stylesheet here vs. running arbitrary JS */}
+      {__scans.hasSnargles && (
+        <link
+          href="https://fonts.googleapis.com/css?family=Press+Start+2P"
+          rel="stylesheet"
+        />
+      )}
 
-        <h1 style={{ fontFamily: "'Press Start 2P'" }}>{frontMatter.title}</h1>
-        <h2 style={{ fontFamily: "'Press Start 2P'" }}>
-          Should render in 8-bit font because...
-        </h2>
-        {__scans.hasSnargles && <h1>We found snargles</h1>}
+      <h1 style={{ fontFamily: "'Press Start 2P'" }}>{frontMatter.title}</h1>
+      <h2 style={{ fontFamily: "'Press Start 2P'" }}>
+        Should render in 8-bit font because...
+      </h2>
+      {__scans.hasSnargles && <h1>We found snargles</h1>}
 
-        {__scans.snarglesName && <h1>{__scans.snarglesName}</h1>}
-        <p>
-          Other docs: {introData.title}, {advancedData.title}
-        </p>
-        {children}
-      </>
-    )
-  }
+      {__scans.snarglesName && <h1>{__scans.snarglesName}</h1>}
+      <p>
+        Other docs: {introData.title}, {advancedData.title}
+      </p>
+      {children}
+    </>
+  )
 }
