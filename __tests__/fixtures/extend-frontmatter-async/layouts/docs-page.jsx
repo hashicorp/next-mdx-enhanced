d59aac7930c1b9e0a486303a657f9545b42e1d98
Jem (Jam) Bezooyen <hey@jem.dev>
2020-08-15 19:08:24 -0700

    fix(tests): Update tests to work with new layouts
    This branch shifted the default layout format to a file that exports a
    single functional component as default (wrather than returning a
    function that THEN returns the component).
    
    Tests however had the old layouts and all need updating
    
    BREAKING CHANGE: Layouts having a new api means they will need updating,
    and currently this doesn't support a layout also doing any sort of custom
    data fetching (such as `getStaticProps` and the like).

--- __tests__/fixtures/extend-frontmatter-async/layouts/docs-page.jsx
+++ __tests__/fixtures/extend-frontmatter-async/layouts/docs-page.jsx
@@ -1,18 +1,16 @@
 import { frontMatter as introData } from '../pages/docs/intro.mdx'
 import { frontMatter as advancedData } from '../pages/docs/advanced.mdx'
 
-export default (frontMatter) => {
-  return function docsPageLayout({ children }) {
-    return (
-      <>
-        <p>LAYOUT TEMPLATE</p>
-        <h1>{frontMatter.title}</h1>
-        <p>All frontMatter: {JSON.stringify(frontMatter)}</p>
-        <p>
-          Other docs: {introData.title}, {advancedData.title}
-        </p>
-        {children}
-      </>
-    )
-  }
+export default function docsPageLayout({ children, frontMatter }) {
+  return (
+    <>
+      <p>LAYOUT TEMPLATE</p>
+      <h1>{frontMatter.title}</h1>
+      <p>All frontMatter: {JSON.stringify(frontMatter)}</p>
+      <p>
+        Other docs: {introData.title}, {advancedData.title}
+      </p>
+      {children}
+    </>
+  )
 }
